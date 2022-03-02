6241141fa0790aad0d0eff2e4fe14c2362565d9d
Jesse Ludeman <jesseludeman@Jesses-MacBook-Pro.local>
2022-03-02 22:26:46 +1000

    Added password_policy block

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 resource "aws_cognito_user_pool" "pool" {
     name = var.cognito_pool_name
+
+    password_policy {
+        minimum_length = var.min_length
+        require_lowercase = var.require_lowercase
+        require_uppercase = var.require_lowercase
+        require_numbers = var.require_numbers
+        require_symbols = var.require_symbols
+        temporary_password_validity = var.temp_password_validity
+    }
 }
